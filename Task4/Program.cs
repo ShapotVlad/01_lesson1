ac80ed42706385e0c695502b78ae580d7c92dd12
Vladimir <vlnshapat@gmail.com>
2024-04-21 19:53:13 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿int a = 44;
+int b = 78;
+int c = 16;
+int d = 23;
+int e = 55;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
+
+
+
