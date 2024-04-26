1486797c8d64853302aefe1718ff1faa04087cf7
Vladimir <vlnshapat@gmail.com>
2024-04-26 20:57:09 +0300

    add

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -4,7 +4,7 @@ int i = 1;
 while (i <= n)
 {
     //Console.WriteLine(i);
-   Console.Write(i);
-   Console.Write(" ");
-       i = i + 1;
+    Console.Write(i);
+    Console.Write(" ");
+    i = i + 1;
 }
