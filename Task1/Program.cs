194a398e1f633181190a370f5474e00c77a8a90f
Vladimir <vlnshapat@gmail.com>
2024-04-28 19:19:51 +0300

    изменили Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,11 +1,16 @@
-﻿//Console.WriteLine("Hello world");
+﻿//ввод чисел в C#
 
-
+long f = 25687; 
+Console.WriteLine(f);
 int value = 100;
 Console.WriteLine(value);
-
+float value2 = 94458;
 double d = 5.31;
+char m = 'a';
 bool e = true;
+bool b = false;
 Console.WriteLine(d);
 Console.WriteLine(e);
-
+Console.WriteLine(value2);
+Console.WriteLine(m);
+Console.WriteLine(b);
