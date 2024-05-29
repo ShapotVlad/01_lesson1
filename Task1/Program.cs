7f74adb5de4e7c2589ea19ba021e3ab0d1b1f365
Vladimir <vlnshapat@gmail.com>
2024-05-29 21:53:57 +0300

    add explanations

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,14 +1,19 @@
-﻿//ввод чисел в C#
+﻿//вывод чисел в C# на консоль
 
-long f = 25687; 
+long f = 25687;  //целый тип длинный
 Console.WriteLine(f);
-int value = 100;
+
+int value = 100; //целый тип
 Console.WriteLine(value);
-float value2 = 94458;
-double d = 5.31;
-char m = 'a';
-bool e = true;
+
+float value2 = 94458; //вещественный тип
+double d = 5.31; //вещественный
+
+char m = 'a';//символьный
+
+bool e = true; //логический
 bool b = false;
+
 Console.WriteLine(d);
 Console.WriteLine(e);
 Console.WriteLine(value2);
