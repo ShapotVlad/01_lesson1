12ae96f5444555ab5a753221669dc6d9f0ce5968
Vladimir <vlnshapat@gmail.com>
2024-05-29 22:08:09 +0300

    add explanations

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,11 +1,14 @@
-﻿int a1 = 9;
+﻿//действие операторов с выводом результата
+
+
+int a1 = 9;
 int a2 = 13;
 
-if (a1>a2)
+if (a1 > a2)
 {
     Console.WriteLine(a1);
 }
 else
 {
-Console.WriteLine(a2);
+    Console.WriteLine(a2);
 }
