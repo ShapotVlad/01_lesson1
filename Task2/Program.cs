98772fdaf22c78c6354a2fb750b737c0f5f0d1e6
Vladimir <vlnshapat@gmail.com>
2024-05-29 22:02:56 +0300

    add explanations

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,10 +1,13 @@
-﻿int i1 = 8;
+﻿// вывод на консоль  арифметических действий
+
+
+int i1 = 8;
 int i2 = 9;
-int sum = i1+i2;
+int sum = i1 + i2;
 Console.WriteLine(sum);
 
 long l1 = 31;
-long sum2= l1+i2;
+long sum2 = l1 + i2;
 Console.WriteLine(sum2);
 
 double d1 = 22.55;
@@ -18,5 +21,5 @@ Console.WriteLine(prod1);
 Console.WriteLine(prod2);
 Console.WriteLine(prod3);
 
-double quot1 = d1/i2;
+double quot1 = d1 / i2;
 Console.WriteLine(quot1);
